93220868a1b1c6efc01a546a00385fe99463dc63
Gabriel <gabrielsz87@gmail.com>
2021-05-14 13:58:20 -0500

    init

---
+++ api/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const productsRouter = require("./products/products.router");
+
+app.use(express.json());
+app.use("/api/ping", (_request, response, _next) => {
+  response.setHeader('Content-Type', 'application/json')
+  response.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  response.json({ data: "pong!" });
+});
+app.use("/api/products", productsRouter);
+
+// Not found handler
+app.use((request, _response, next) => {
+  next({ status: 404, message: `Not found: ${request.originalUrl}` });
+});
+
+// Error handler
+app.use((error, _request, response, _next) => {
+  console.error(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  response.status(status).json({ errors: [message] });
+});
+
+const { PORT = 5000 } = process.env;
+const listener = () => console.log(`Listening on Port ${PORT}!`);
+app.listen(PORT, listener);
+
+module.exports = app;
---
+++ knexfile.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const {
+  NODE_ENV = "development",
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
+} = process.env;
+const URL =
+  NODE_ENV === "production"
+    ? PRODUCTION_DATABASE_URL
+    : DEVELOPMENT_DATABASE_URL;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: URL,
+    migrations: {
+      directory: __dirname + "/src/api/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/src/api/db/seeds",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: URL,
+    migrations: {
+      directory: __dirname + "/src/api/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/src/api/db/seeds",
+    },
+  },
+};
